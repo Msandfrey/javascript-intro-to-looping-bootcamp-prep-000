7f28cb54c9f7cd4375f445e730249c695225a695
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 21:29:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(array){
   for(i = 0; i < 25; i++){
-    array.push(`I am ${i} strange loop${i === 1 ? '': 's'}.`)
+    var s = i===1 ? "" : "s"
+    array.push(`I am ${i} strange loop${s}.`)
   }
 }
