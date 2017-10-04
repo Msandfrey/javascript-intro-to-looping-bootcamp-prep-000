7bb8f91f6dbd837d78db519ea940590d5d5d0512
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 21:51:19 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array){
+  for(i = 0; i < 25; i++){
+    var s = i===1 ? "" : "s"
+    array.push(`I am ${i} strange loop${s}.`)
+  }
+  return array
+}
+function whileLoop(n){
+  while(n>0){
+    console.log(n)
+    n--
+  }
+  return "done"
+}
+function doWhileLoop(array){
+  do{
+    array.pop()
+  }while(array.length > 0 || maybeTrue())
+  return array
+}
+function maybeTrue(){
+  return Math.random >= 0.5;
+}
